a7038ce0a91e48d7e4f87ad60945c7f99b99b73e
Abdullah Khan <85450222+AbdullahDaGoat@users.noreply.github.com>
2023-11-23 15:09:02 +0000

    Replys to modmail!

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import praw
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Load your environment variables
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 username = os.getenv("username")
 password = os.getenv("password")
 
+# Create a Reddit instance
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
@@ -19,11 +22,23 @@ reddit = praw.Reddit(
 )
 
 subreddit_name = "roomba"  # Name of the subreddit you want to monitor
-
 subreddit = reddit.subreddit(subreddit_name)
 
-for modmail in subreddit.modmail.stream():
-    if modmail.unread:
-        modmail.reply("hello")  # Modify the message content as needed
-        modmail.mark_read()
-        modmail.archive()
+processed_modmail_ids = []  # List to store IDs of processed modmails
+
+while True:
+    for conversation in subreddit.modmail.conversations(state="new"):
+        for modmail in conversation.messages:
+            author_name = modmail.author.name if modmail.author else None
+            if modmail.id not in processed_modmail_ids and author_name != subreddit_name:
+                # Reply to the modmail
+                conversation.reply(body="Hello! Your message is queued.")
+
+                # Archive the modmail
+                conversation.archive()
+
+                # Add the modmail ID to the processed list
+                processed_modmail_ids.append(modmail.id)
+
+    # Delay between checking for new modmails
+    time.sleep(300)  # Delay for 300 seconds (5 minutes) before checking again
